2aefb1c94e44709fc3d282c6b091ddb6bda1df3d
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-20 10:53:03 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// write code here
+const state1 = { username: 'john', point: 100, loading: true };
+let state2 = {};
+
+for (let key in state1) {
+  Object.assign(state2, state1, { point: 75, loading: false, success: true })
+}
+
+console.log('state1',state1,'state2', state2)
